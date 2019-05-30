0d15ffb0ff170e86b01bca33869fb25b33fc8649
James Carlson <jxxcarlson@mac.com>
2019-05-30 09:58:16 -0400

    Reflect all recruiting changes to the graph in the grid

--- src/Grid.elm
+++ src/Grid.elm
@@ -1,14 +1,19 @@
-module Grid exposing(Cell, CellStatus(..), empty, cellGridFromGraph)
+module Grid exposing (Cell, CellStatus(..), empty, recruitedCount, cellGridFromGraph)
 
 import Network
 import Graph exposing (Edge, Graph, Node, NodeContext, NodeId)
-import CellGrid exposing(CellGrid)
+import CellGrid exposing (CellGrid(..))
 import Array
 
 
 -- type alias NodeState = { name: String, status: Status, location: (Int, Int) }
 
-type CellStatus  = Recruited | NotRecruited | Vacant
+
+type CellStatus
+    = Recruited
+    | NotRecruited
+    | Vacant
+
 
 type alias Cell =
     { status : CellStatus
@@ -16,36 +21,54 @@ type alias Cell =
     , id : Int
     }
 
+
 cellFromEntity : Network.Entity -> Cell
 cellFromEntity entity =
-   let nodeState_ = Network.nodeState entity
-   in
-     case nodeState_.status of
-       Network.Recruited -> { id = entity.id, name = nodeState_.name, status = Recruited}
-       Network.NotRecruited -> { id = entity.id, name = nodeState_.name, status = NotRecruited}
+    let
+        nodeState_ =
+            Network.nodeState entity
+    in
+        case nodeState_.status of
+            Network.Recruited ->
+                { id = entity.id, name = nodeState_.name, status = Recruited }
+
+            Network.NotRecruited ->
+                { id = entity.id, name = nodeState_.name, status = NotRecruited }
 
 
-empty: Int -> Int -> CellGrid Cell
+empty : Int -> Int -> CellGrid Cell
 empty rows columns =
-    CellGrid.fromList rows columns (List.repeat (rows*columns) {id = -1, name = "", status = Vacant})
-      |> Maybe.withDefault CellGrid.empty
+    CellGrid.fromList rows columns (List.repeat (rows * columns) { id = -1, name = "", status = Vacant })
+        |> Maybe.withDefault CellGrid.empty
+
+
+recruitedCount : CellGrid Cell -> Int
+recruitedCount (CellGrid _ cellArray) =
+    Array.foldl
+        (\cell acc ->
+            if cell.status == Recruited then
+                acc + 1
+            else
+                acc
+        )
+        0
+        cellArray
 
 
 cellGridFromGraph : Int -> Graph Network.Entity () -> CellGrid Cell
 cellGridFromGraph gridWidth graph =
     let
-        emptyCellGrid = empty gridWidth gridWidth
-     in
+        emptyCellGrid =
+            empty gridWidth gridWidth
+    in
         List.foldl insertNode emptyCellGrid (Graph.nodes graph)
 
 
-insertNode : (Graph.Node Network.Entity) -> CellGrid Cell -> CellGrid Cell
+insertNode : Graph.Node Network.Entity -> CellGrid Cell -> CellGrid Cell
 insertNode nodeEntity grid =
     let
-      -- (Graph.Node entity) = nodeEntity
-      nodeState_ = Network.nodeState nodeEntity.label
+        -- (Graph.Node entity) = nodeEntity
+        nodeState_ =
+            Network.nodeState nodeEntity.label
     in
-    CellGrid.setValue grid nodeState_.location (cellFromEntity nodeEntity.label)
-
-
-
+        CellGrid.setValue grid nodeState_.location (cellFromEntity nodeEntity.label)
--- src/Network.elm
+++ src/Network.elm
@@ -1,8 +1,30 @@
-module Network exposing (Entity, Status(..), NodeState, defaultNodeState, connect, setStatus
-  , hiddenTestGraph, testGraph, initializeNode, updateContextWithValue, outGoingNodeIds, inComingNodeIds
-  , connectNodeToNodeInList, setupGraph, computeForces, influencees, influencers, influencees2, recruitNodes,
-  nodeComplementOfGraph, randomListElement, randomPairs, integerSequence, nodeState)
-
+module Network
+    exposing
+        ( Entity
+        , Status(..)
+        , NodeState
+        , defaultNodeState
+        , connect
+        , setStatus
+        , hiddenTestGraph
+        , testGraph
+        , initializeNode
+        , updateContextWithValue
+        , outGoingNodeIds
+        , inComingNodeIds
+        , connectNodeToNodeInList
+        , setupGraph
+        , computeForces
+        , influencees
+        , influencers
+        , influencees2
+        , recruitNodes
+        , nodeComplementOfGraph
+        , randomListElement
+        , randomPairs
+        , integerSequence
+        , nodeState
+        )
 
 import Force exposing (State)
 import Graph exposing (Edge, Graph, Node, NodeContext, NodeId)
@@ -15,20 +37,30 @@ import List.Extra
 -- TYPES
 --
 
-type alias NodeState = { name: String, status: Status, location: (Int, Int) }
 
-type Status = Recruited | NotRecruited
+type alias NodeState =
+    { name : String, status : Status, location : ( Int, Int ) }
+
+
+type Status
+    = Recruited
+    | NotRecruited
+
 
 type alias Entity =
     Force.Entity NodeId { value : NodeState }
+
 
 nodeState : Entity -> NodeState
 nodeState entity =
     entity.value
 
+
+
 --
 -- INITIALIZATION
 --
+
 
 distributeLocations : Graph Entity () -> Graph Entity ()
 distributeLocations graph =
@@ -37,48 +69,67 @@ distributeLocations graph =
 
 integerSequence : Int -> Int -> Int -> List Int
 integerSequence modulus n seed =
-   PseudoRandom.floatSequence n seed (0,1)
-     |> List.map (\x -> round ((toFloat modulus)*x))
-     |> List.Extra.unique
-     |> List.filter (\x -> x < modulus)
+    PseudoRandom.floatSequence n seed ( 0, 1 )
+        |> List.map (\x -> round ((toFloat modulus) * x))
+        |> List.Extra.unique
+        |> List.filter (\x -> x < modulus)
 
-randomPairs:  Int -> Int -> Int -> List (Int, Int)
+
+randomPairs : Int -> Int -> Int -> List ( Int, Int )
 randomPairs modulus n seed =
-    List.map2 Tuple.pair (integerSequence modulus n seed) (integerSequence modulus n (seed*seed))
+    List.map2 Tuple.pair (integerSequence modulus n seed) (integerSequence modulus n (seed * seed))
 
-unrecruitedNodeState : String -> (Int, Int) -> NodeState
-unrecruitedNodeState name (i,j) =
-    { name = name, status = NotRecruited, location = (i,j) }
 
-recruitedNodeState : String -> (Int, Int) -> NodeState
-recruitedNodeState name (i,j) =
-    { name = name, status = Recruited, location = (i,j) }
+unrecruitedNodeState : String -> ( Int, Int ) -> NodeState
+unrecruitedNodeState name ( i, j ) =
+    { name = name, status = NotRecruited, location = ( i, j ) }
+
+
+recruitedNodeState : String -> ( Int, Int ) -> NodeState
+recruitedNodeState name ( i, j ) =
+    { name = name, status = Recruited, location = ( i, j ) }
 
 
 hiddenTestGraph =
     Graph.fromNodeLabelsAndEdgePairs
-        [ unrecruitedNodeState "p0" (10,18), unrecruitedNodeState "12" (16,15)
-        , unrecruitedNodeState "p2" (13,17), unrecruitedNodeState "p3" (3,12)
-        , unrecruitedNodeState "p4" (19,19), unrecruitedNodeState "p5" (2,1)
-        , unrecruitedNodeState "q0" (9,3), unrecruitedNodeState "q1"(7,13)
-        , unrecruitedNodeState "q2" (11,11), unrecruitedNodeState "q3" (4,6)
-        , unrecruitedNodeState "q4" (18,2), unrecruitedNodeState "q5"(17,8)
-        , recruitedNodeState "r" (8,10) ]
+        [ unrecruitedNodeState "p0" ( 10, 18 )
+        , unrecruitedNodeState "12" ( 16, 15 )
+        , unrecruitedNodeState "p2" ( 13, 17 )
+        , unrecruitedNodeState "p3" ( 3, 12 )
+        , unrecruitedNodeState "p4" ( 19, 19 )
+        , unrecruitedNodeState "p5" ( 2, 1 )
+        , unrecruitedNodeState "q0" ( 9, 3 )
+        , unrecruitedNodeState "q1" ( 7, 13 )
+        , unrecruitedNodeState "q2" ( 11, 11 )
+        , unrecruitedNodeState "q3" ( 4, 6 )
+        , unrecruitedNodeState "q4" ( 18, 2 )
+        , unrecruitedNodeState "q5" ( 17, 8 )
+        , recruitedNodeState "r" ( 8, 10 )
+        ]
         [ ( 0, 1 ), ( 0, 2 ), ( 0, 3 ), ( 0, 4 ), ( 0, 5 ), ( 6, 7 ), ( 6, 8 ), ( 6, 9 ), ( 6, 10 ), ( 6, 11 ) ]
 
 
 testGraph =
     Graph.fromNodeLabelsAndEdgePairs
-         [ unrecruitedNodeState "p0" (10,18), unrecruitedNodeState "12" (16,15)
-                 , unrecruitedNodeState "p2" (13,17), unrecruitedNodeState "p3" (3,12)
-                 , unrecruitedNodeState "p4" (19,19), unrecruitedNodeState "p5" (2,1)
-                 , unrecruitedNodeState "q0" (9,3), unrecruitedNodeState "q1"(7,13)
-                 , unrecruitedNodeState "q2" (11,11), unrecruitedNodeState "q3" (4,6)
-                 , unrecruitedNodeState "q4" (18,2), unrecruitedNodeState "q5"(17,8)
-                 , recruitedNodeState "r" (8,10) ]
-        [  ]
+        [ unrecruitedNodeState "p0" ( 10, 18 )
+        , unrecruitedNodeState "12" ( 16, 15 )
+        , unrecruitedNodeState "p2" ( 13, 17 )
+        , unrecruitedNodeState "p3" ( 3, 12 )
+        , unrecruitedNodeState "p4" ( 19, 19 )
+        , unrecruitedNodeState "p5" ( 2, 1 )
+        , unrecruitedNodeState "q0" ( 9, 3 )
+        , unrecruitedNodeState "q1" ( 7, 13 )
+        , unrecruitedNodeState "q2" ( 11, 11 )
+        , unrecruitedNodeState "q3" ( 4, 6 )
+        , unrecruitedNodeState "q4" ( 18, 2 )
+        , unrecruitedNodeState "q5" ( 17, 8 )
+        , recruitedNodeState "r" ( 8, 10 )
+        ]
+        []
 
-defaultNodeState = { name = "", status = NotRecruited, location = (0,0) }
+
+defaultNodeState =
+    { name = "", status = NotRecruited, location = ( 0, 0 ) }
 
 
 initializeNode : NodeContext NodeState () -> NodeContext Entity ()
@@ -89,23 +140,25 @@ initializeNode ctx =
     }
 
 
-setupGraph : Graph.Graph NodeState ()
-             -> ( List (Force.Force Int), Graph.Graph Entity () )
+setupGraph :
+    Graph.Graph NodeState ()
+    -> ( List (Force.Force Int), Graph.Graph Entity () )
 setupGraph inputGraph =
     let
-            outputGraph =
-                Graph.mapContexts initializeNode inputGraph
+        outputGraph =
+            Graph.mapContexts initializeNode inputGraph
 
-            link { from, to } =
-                ( from, to )
+        link { from, to } =
+            ( from, to )
 
-            forces =
-                [ Force.links <| List.map link <| Graph.edges outputGraph
-                , Force.manyBody <| List.map .id <| Graph.nodes outputGraph
-                , Force.center (500 / 2) (500 / 2)
-                ]
-     in
-       (forces, outputGraph)
+        forces =
+            [ Force.links <| List.map link <| Graph.edges outputGraph
+            , Force.manyBody <| List.map .id <| Graph.nodes outputGraph
+            , Force.center (500 / 2) (500 / 2)
+            ]
+    in
+        ( forces, outputGraph )
+
 
 
 --
@@ -114,10 +167,15 @@ setupGraph inputGraph =
 
 
 setStatus : Int -> Status -> Graph Entity () -> Graph Entity ()
-setStatus  nodeIndex status graph =
-    Graph.mapNodes (\n -> if n.id == nodeIndex then
-         { n | value = { name = n.value.name, status = status, location = n.value.location }}
-      else n) graph
+setStatus nodeIndex status graph =
+    Graph.mapNodes
+        (\n ->
+            if n.id == nodeIndex then
+                { n | value = { name = n.value.name, status = status, location = n.value.location } }
+            else
+                n
+        )
+        graph
 
 
 updateContextWithValue : NodeContext Entity () -> Entity -> NodeContext Entity ()
@@ -129,28 +187,39 @@ updateContextWithValue nodeCtx value =
         { nodeCtx | node = { node | label = value } }
 
 
+
 --
 -- CONNECT
 --
 
-connect: NodeId -> NodeId -> Graph n () -> Graph n ()
+
+connect : NodeId -> NodeId -> Graph n () -> Graph n ()
 connect from to graph =
     case newContext from to graph of
-        Nothing -> graph
-        Just ctx -> Graph.insert ctx graph
+        Nothing ->
+            graph
 
-connectNodeToNodeInList : NodeId -> List NodeId -> Graph n () -> Graph n ()
+        Just ctx ->
+            Graph.insert ctx graph
+
+
+connectNodeToNodeInList : NodeId -> List NodeId -> Graph Entity () -> Graph Entity ()
 connectNodeToNodeInList from nodeList graph =
-    List.foldl (\to graph_ -> connect from to graph_) graph nodeList
+    List.foldl (\to graph_ -> connect from to graph_ |> setStatus to Recruited) graph nodeList
 
 
-newContext: NodeId -> NodeId -> Graph n () -> Maybe (NodeContext n ())
+
+-- xxx setStatus nodeIndex status graph
+
+
+newContext : NodeId -> NodeId -> Graph n () -> Maybe (NodeContext n ())
 newContext from to graph =
-    Maybe.map (\x -> { x | outgoing = IntDict.insert to () x.outgoing} ) (Graph.get from graph)
+    Maybe.map (\x -> { x | outgoing = IntDict.insert to () x.outgoing }) (Graph.get from graph)
 
-newContext2: NodeId -> NodeId -> Graph n () -> Maybe (NodeContext n ())
+
+newContext2 : NodeId -> NodeId -> Graph n () -> Maybe (NodeContext n ())
 newContext2 from to graph =
-    Maybe.map (\x -> { x | outgoing = IntDict.insert to () x.outgoing} ) (Graph.get from graph)
+    Maybe.map (\x -> { x | outgoing = IntDict.insert to () x.outgoing }) (Graph.get from graph)
 
 
 
@@ -158,18 +227,25 @@ newContext2 from to graph =
 -- CONNECTIONS
 --
 
+
 outGoingNodeIds : NodeId -> Graph n e -> List NodeId
 outGoingNodeIds nodeId graph =
     case Graph.get nodeId graph of
-        Nothing -> []
-        Just ctx -> ctx.outgoing |> IntDict.keys
+        Nothing ->
+            []
+
+        Just ctx ->
+            ctx.outgoing |> IntDict.keys
 
 
 inComingNodeIds : NodeId -> Graph n e -> List NodeId
 inComingNodeIds nodeId graph =
     case Graph.get nodeId graph of
-        Nothing -> []
-        Just ctx -> ctx.incoming |> IntDict.keys
+        Nothing ->
+            []
+
+        Just ctx ->
+            ctx.incoming |> IntDict.keys
 
 
 
@@ -183,73 +259,88 @@ inComingNodeIds nodeId graph =
 influencees : NodeId -> Graph n e -> List NodeId
 influencees nodeId graph =
     Maybe.map Graph.alongOutgoingEdges (Graph.get nodeId graph)
-       |> Maybe.withDefault []
+        |> Maybe.withDefault []
+
 
 {-| If graph contains a <- b, a <- c, then influencers a graph returns [b,c]
 -}
 influencers : NodeId -> Graph n e -> List NodeId
 influencers nodeId graph =
     Maybe.map Graph.alongIncomingEdges (Graph.get nodeId graph)
-       |> Maybe.withDefault []
+        |> Maybe.withDefault []
+
 
 {-| If graph contains a <- b -> c, then influencers2 a graph returns [c]
 -}
 influencees2 : NodeId -> Graph n e -> List NodeId
 influencees2 nodeId graph =
-   List.map (\n -> influencees n graph) (influencers nodeId graph)
-     |> List.concat
-     |> List.filter (\x -> x /= nodeId)
-
+    List.map (\n -> influencees n graph) (influencers nodeId graph)
+        |> List.concat
+        |> List.filter (\x -> x /= nodeId)
 
 
 influencees2b : NodeId -> Graph n e -> List NodeId
 influencees2b nodeId graph =
     influencers nodeId graph ++ influencees2 nodeId graph
 
+
 scale : Float -> Int -> Int
 scale x n =
-  round(x * (toFloat n))
+    round (x * (toFloat n))
 
 
 randomListElement : Maybe Float -> List a -> Maybe a
 randomListElement maybeRandomNumber list =
     case maybeRandomNumber of
-        Nothing -> Nothing
+        Nothing ->
+            Nothing
+
         Just rn ->
             let
-              n = List.length list
-              i =  scale rn (n - 1)
+                n =
+                    List.length list
+
+                i =
+                    scale rn (n - 1)
             in
-               List.Extra.getAt i list
+                List.Extra.getAt i list
 
 
 nodeComplementOfGraph : Graph n e -> List NodeId -> List NodeId
 nodeComplementOfGraph graph nodeExclusionList =
     Graph.nodeIds graph
-      |> List.Extra.filterNot (\item -> List.member item nodeExclusionList)
+        |> List.Extra.filterNot (\item -> List.member item nodeExclusionList)
 
 
-recruitNodes : List Float -> NodeId -> Graph n () -> Graph n () -> Graph n ()
+recruitNodes : List Float -> NodeId -> Graph Entity () -> Graph Entity () -> Graph Entity ()
 recruitNodes rnList recruiterNode currentGraph hiddenGraph_ =
-  let
-     randomInfluenceeNodeId : Maybe NodeId
-     randomInfluenceeNodeId =
-        influencees recruiterNode currentGraph
-          |> randomListElement (List.Extra.getAt 0 rnList)
+    let
+        randomInfluenceeNodeId : Maybe NodeId
+        randomInfluenceeNodeId =
+            influencees recruiterNode currentGraph
+                |> randomListElement (List.Extra.getAt 0 rnList)
 
-     secondOrderInfluencees : List NodeId
-     secondOrderInfluencees = Maybe.map (\i -> influencees2b i hiddenGraph_) randomInfluenceeNodeId |> Maybe.withDefault []
+        secondOrderInfluencees : List NodeId
+        secondOrderInfluencees =
+            Maybe.map (\i -> influencees2b i hiddenGraph_) randomInfluenceeNodeId |> Maybe.withDefault []
 
-     randomSecondOrderInfluenceeNodeId : Maybe NodeId
-     randomSecondOrderInfluenceeNodeId =
-        secondOrderInfluencees
-          |> randomListElement (List.Extra.getAt 0 rnList)
-  in
-  case randomSecondOrderInfluenceeNodeId of
-     Nothing -> currentGraph
-     Just newNodeId -> connect recruiterNode newNodeId currentGraph
+        randomSecondOrderInfluenceeNodeId : Maybe NodeId
+        randomSecondOrderInfluenceeNodeId =
+            secondOrderInfluencees
+                |> randomListElement (List.Extra.getAt 0 rnList)
+    in
+        case randomSecondOrderInfluenceeNodeId of
+            Nothing ->
+                currentGraph
+
+            Just newNodeId ->
+                connect recruiterNode newNodeId currentGraph
+                    |> setStatus newNodeId Recruited
 
 
+
+-- |> setStatus newNodeId Recruited
+-- Network.setStatus index Recruited model.graph
 --
 -- FORCES
 --
@@ -258,20 +349,21 @@ recruitNodes rnList recruiterNode currentGraph hiddenGraph_ =
 computeForces : Graph.Graph n e -> List (Force.Force Int)
 computeForces graph =
     let
-        k = 2.0
+        k =
+            2.0
+
         link { from, to } =
-                    ( from,  to )
+            ( from, to )
     in
-        [  Force.customLinks 1 <| List.map alterLink <| List.map link <| Graph.edges graph
-         , Force.manyBodyStrength -1.8 (List.map .id <| Graph.nodes graph)
-         ,  Force.center (500 / 2) (500 / 2)
+        [ Force.customLinks 1 <| List.map alterLink <| List.map link <| Graph.edges graph
+        , Force.manyBodyStrength -1.8 (List.map .id <| Graph.nodes graph)
+        , Force.center (500 / 2) (500 / 2)
         ]
 
 
-alterLink (from, to) =
+alterLink ( from, to ) =
     { source = from
     , target = to
     , distance = 90
     , strength = Just 1
-
     }
